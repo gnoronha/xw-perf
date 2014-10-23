0a2002c4c0076121ed715e1d236f5191d337810c
Pekka Paalanen <pekka.paalanen@collabora.co.uk>
2014-10-23 09:53:56 +0300

    social-java: more dummy items and reindent
    Generate lots of dummy items to the list.
    Drop text color setting to white.
    Reindent in Eclipse.

--- java_social/src/com/collabora/xwperf/social/java/MainActivity.java
+++ java_social/src/com/collabora/xwperf/social/java/MainActivity.java
@@ -1,5 +1,7 @@
 package com.collabora.xwperf.social.java;
 
+import java.util.ArrayList;
+
 import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
@@ -15,84 +17,89 @@ import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
-	String[] items = { "abba", "bebba", "cucca", "toffo" };
-	
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        
-        ListView list = (ListView)findViewById(R.id.list);
-        
-        SpecialAdapter adapter = new SpecialAdapter(this, items);
-        list.setAdapter(adapter);
-    }
+	String[] names = { "abba", "bebba", "cucca", "toffo" };
 
-    static class ViewHolder {
-    	TextView text;
-    }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
+		ListView list = (ListView)findViewById(R.id.list);
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+		ArrayList<String> items = new ArrayList<String>();
+		for (int i = 0; i < 100; i++) {
+			items.add(String.format("%s%d", names[i % names.length], i));
+		}
 
-    private class SpecialAdapter extends BaseAdapter {
-    	private LayoutInflater mInflater;
-    	private String[] data;
-    	
-    	public SpecialAdapter(Context context, String[] items) {
-    		mInflater = LayoutInflater.from(context);
-    		this.data = items;
-    	}
-    	
-    	@Override
-    	public int getCount() {
-    		return data.length;
-    	}
-    	
-    	@Override
-    	public Object getItem(int position) {
-    		return position;
-    	}
-    	
-    	@Override
-    	public long getItemId(int position) {
-    		return position;
-    	}
-    	
-    	@Override
-    	public View getView(int position, View convertView, ViewGroup parent) {
-    		ViewHolder holder;
-    		
-    		if (convertView == null) {
-    			convertView = mInflater.inflate(R.layout.row, null);
-    			
-    			holder = new ViewHolder();
-    			holder.text = (TextView)convertView.findViewById(R.id.headline);
-    			convertView.setTag(holder);
-    		} else {
-    			holder = (ViewHolder)convertView.getTag();
-    		}
-    		
-    		holder.text.setText(data[position]);
-    		
-    		return convertView;
-    	}
-    }
+		SpecialAdapter adapter = new SpecialAdapter(this, items);
+		list.setAdapter(adapter);
+	}
+
+	static class ViewHolder {
+		TextView text;
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+	private class SpecialAdapter extends BaseAdapter {
+		private LayoutInflater mInflater;
+		private ArrayList<String> data;
+
+		public SpecialAdapter(Context context, ArrayList<String> items) {
+			mInflater = LayoutInflater.from(context);
+			this.data = items;
+		}
+
+		@Override
+		public int getCount() {
+			return data.size();
+		}
+
+		@Override
+		public Object getItem(int position) {
+			return position;
+		}
+
+		@Override
+		public long getItemId(int position) {
+			return position;
+		}
+
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+			ViewHolder holder;
+
+			if (convertView == null) {
+				convertView = mInflater.inflate(R.layout.row, null);
+
+				holder = new ViewHolder();
+				holder.text = (TextView)convertView.findViewById(R.id.headline);
+				convertView.setTag(holder);
+			} else {
+				holder = (ViewHolder)convertView.getTag();
+			}
+
+			holder.text.setText(data.get(position));
+
+			return convertView;
+		}
+	}
 
 }
