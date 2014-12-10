df52b7cd5023ab187124c964cac854ad81ce9292
Simon McVittie <simon.mcvittie@collabora.co.uk>
2014-12-10 19:20:04 +0000

    Add a script wrapping get_stats.py to capture test results

---
+++ capture.sh
@@ -0,0 +1,61 @@
+#!/bin/sh
+
+set -e
+
+CURDIR="$(dirname "$0")"
+[ -n "${CURDIR}" ] || CURDIR="."
+
+usage () {
+	echo "usage: ${CURDIR}/capture.sh [x|n] [s|c] [test-case]" >&2
+}
+
+runtime=
+app=
+surface=SurfaceView
+
+case "$2" in
+	(s)
+		app=social
+		;;
+	(c)
+		app=contacts
+		;;
+	(*)
+		usage
+		exit 2
+		;;
+esac
+
+case "$1" in
+	(x)
+		runtime="xwperf"
+		;;
+	(n)
+		runtime="notxw"
+		surface="com.collabora.xwperf.notxw_${app}/com.collabora.xwperf.notxw_${app}.MainActivity_"
+		;;
+	(*)
+		usage
+		exit 2
+		;;
+esac
+
+test="$3"
+
+echo "You should have opened ${runtime}_${app} already."
+echo "Start doing whatever the test case is..."
+sleep 1
+beep || printf '\a'
+echo "Starting to collect stats..."
+"$CURDIR"/get_stats.py \
+	--time=5 \
+	--surface="${surface}" \
+	--output="$CURDIR"/results/${runtime}_${app}_${test}.log \
+	> "$CURDIR"/results/${runtime}_${app}_${test}.orig.txt
+echo "OK, you can stop now"
+beep || printf '\a'
+
+"$CURDIR"/get_stats.py \
+	--input="$CURDIR"/results/${runtime}_${app}_${test}.log \
+	--graph="$CURDIR"/results/${runtime}_${app}_${test}.svg \
+	> "$CURDIR"/results/${runtime}_${app}_${test}.txt
