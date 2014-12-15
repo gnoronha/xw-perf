4441115a5a0fc79ddfc1b1d8870b9fb01cb6b988
Simon McVittie <simon.mcvittie@collabora.co.uk>
2014-12-15 16:47:06 +0000

    Add more timestamp constants

--- notxw_social/perf/src/main/java/com/collabora/xwperf/fps_measure_module/MeasurementLogger.java
+++ notxw_social/perf/src/main/java/com/collabora/xwperf/fps_measure_module/MeasurementLogger.java
@@ -45,8 +45,11 @@ public class MeasurementLogger {
         public static final String MARK_PERF_COLLECTOR_ATTACH_BLINKENLIGHT = "mark_perf_collector_attach_blinkenlight";
         public static final String MARK_PERF_COLLECTOR_SETUP_END = "mark_perf_collector_setup_end";
         public static final String MARK_PERF_COLLECTOR_SETUP_START = "mark_perf_collector_setup_start";
+        public static final String MARK_FEED_LOAD_BEGIN = "measure_feed_load_begin_";
+        public static final String MARK_FEED_LOAD_END = "measure_feed_load_end_";
         public static final String MEASURE_FEED_LOAD = "measure_feed_load_";
         public static final String MEASURE_FILE_READ = "measure_file_read";
+        public static final String MEASURE_FILE_WRITE = "measure_file_write";
     }
 
 }
